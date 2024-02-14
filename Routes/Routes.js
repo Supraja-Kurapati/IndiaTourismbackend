2e799d3a2efda1b85e3d637d3842caca47e80f2f
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-14 14:41:07 +0530

    first

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,6 +1,5 @@
 const express= require('express')
 const router=express.Router();
-const User=require('../Model/UserModel')
 const {register,login} =require ('../Controller/Api')
 
 router.post('/register',register)
--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app=express();
 const cors=require('cors')
 const connectDb=require('./Config/db')
 const router=require('./Routes/Routes')
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(router)
 
 app.use(cors({
     origin:'*'
 }))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use('pages',router)
+
 
 
 app.listen(5031,()=>{
