544fdcc31c9af3a14df9bf6a1bf220ec9dda4560
Supraja-Kurapati <kurapatisupraja8@gmail.com>
2024-02-14 18:31:05 +0530

    loginand register

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,12 +1,20 @@
 const express= require('express')
 const router=express.Router();
-const {register,login} =require ('../Controller/Api')
+const {register,login} =require ('../Controller/Api');
+const auth = require('../MiddleWare/Auth');
 
 router.post('/register',register)
 
 
  router.post('/login',login)
 
+router.post('/TourOffers',auth,(req,res)=>{
+    res.send({msg:"you can acces Tour Offers"})
+})
 
+
+router.post('/TourPackages',auth,(req,res)=>{
+    res.send({msg:"you can acces Tour Packages"})
+})
 
 module.exports=router
